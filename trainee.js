dacb1260116b3f8b8089223f1c29f7f485b7b54d
SHiva Bhusal <cba.bhusal@gmail.com>
2014-07-09 13:34:29 +0545

    delete completed

--- trainee.js
+++ trainee.js
@@ -1,54 +1,98 @@
 function Trainee()
 {
+	this.id;
 	this.name="";
 	this.age="";
 	this.address="";
 }
-var trainees=[];
-var t1=new Trainee;
-t1.name="Shiva Bhusal";t1.age=18;t1.address="Rupandehi";
-trainees.push(t1);
- t1=new Trainee;
-t1.name="Ajay Sapkota";t1.age=25;t1.address="Chitwan";
-trainees.push(t1);
- t1=new Trainee;
-t1.name="Shiva Bhusal";t1.age=18;t1.address="Rupandehi";
-trainees.push(t1);
- t1=new Trainee;
-t1.name="Shiva Bhusal";t1.age=18;t1.address="Rupandehi";
-trainees.push(t1);
-console.log(trainees);
+	//CREATE A DATABASE
+	var trainees=[];
+	//CREATE A NEW INSTANCE OF OBJECT
+		var t1=new Trainee;
+			t1.id=1;t1.name="Shiva Bhusal";t1.age=18;t1.address="Rupandehi";
+			trainees.push(t1);
 
-function ddel(inn)
-{
-console.log(inn);
-trainees[inn].splice;
-}
+		t1=new Trainee;
+			t1.id=2;t1.name="Ajay Sapkota";t1.age=25;t1.address="Chitwan";
+			trainees.push(t1);
+			
+		t1=new Trainee;
+			t1.id=3;t1.name="Shiva Bhusal";t1.age=18;t1.address="Rupandehi";
+			trainees.push(t1);
+			
+		t1=new Trainee;
+			t1.id=4;t1.name="Shiva Bhusal";t1.age=18;t1.address="Rupandehi";
+			trainees.push(t1);
+			
+	console.log(trainees);
+
+
 for(var i=0;i<trainees.length;i++)
 {
 	var tr=document.createElement('tr');
+		tr.id="row-"+(i+1);
 	var td1=document.createElement('td');
 	var td2=document.createElement('td');
 	var td3=document.createElement('td');
 	var td4=document.createElement('td');
 	var td5=document.createElement('td');
+	var deleteButton=document.createElement('a');
+		deleteButton.setAttribute('uid',trainees[i].id);
+		deleteButton.className="delete-button";
+		
+		deleteButton.setAttribute('href','#');
+		deleteButton.innerHTML="Delete";
+		deleteButton.onclick=deleteItem;
+	var editButton=document.createElement('a');
+		editButton.setAttribute('uid',trainees[i].id);
+		editButton.className="edit-button";
+		editButton.onclick=editItem;
+		editButton.setAttribute('href','#');
+		editButton.innerHTML="Edit";
+		
 	td1.innerHTML=i+1;
 	td2.innerHTML=trainees[i].name;
 	td3.innerHTML=trainees[i].age;
 	td4.innerHTML=trainees[i].address;
-	
-	var a=document.createElement('a');
-		a.setAttribute('index',i);
-		a.onclick=function(){alert("Deleting data of "+ trainees[this.getAttribute('index')].name);}
-		a.setAttribute('href','#');
-		a.innerHTML="Delete";
-		
-	td5.appendChild(a);
-	
-	tr.appendChild(td1);tr.appendChild(td2);tr.appendChild(td3);tr.appendChild(td4);
+	td5.appendChild(deleteButton);
+	td5.appendChild(editButton);
+
+	tr.appendChild(td1);
+	tr.appendChild(td2);
+	tr.appendChild(td3);
+	tr.appendChild(td4);
 	tr.appendChild(td5);
 	
 	document.getElementById('tblTrainees').appendChild(tr);
 	
 	
+	
 }
+function findItemById(uid)
+{
+	for(i=0;i<trainees.length;i++)
+	{
+		if(trainees[i].id==uid)
+		{
+			return i;
+		}
+	}
+}
+
+function deleteItem()
+{
+	curUID=this.getAttribute('uid');
+	curIndex=findItemById(curUID);
+	selItem=trainees[curIndex];
+	
+	//ALERT THAT DATA ARE BEING DELETED
+	console.log("Deleting data of "+selItem.name);
+	//DELETE FROM ARRAY
+	trainees.splice(curIndex,1);
+	//DELETE FROM DOM
+		var delItem=document.getElementById("row-"+curUID);
+		delItem.remove();
+			
+	console.log(trainees);
+}
+function editItem(){}
